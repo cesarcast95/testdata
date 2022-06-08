ae043ee2877b6849c057529e4f80331bb82e2e4f
Cesar Castillo <cesarzc95@gmail.com>
2022-06-08 16:03:12 -0500

    se elimino archivos basura

--- testdata/api/testdata_backend/data.py
+++
@@ -1,6 +0,0 @@
-lista = [
-    {"name": "pc", "price": 800, "quantity": 4},
-    {"name": "laptop", "price": 1200, "quantity": 1},
-    {"name": "mouse", "price": 40, "quantity": 6},
-    {"name": "pc", "price": 600, "quantity": 1}
-]
