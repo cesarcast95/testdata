4adf723628be69e7113733daa219a72dec305cdc
Cesar Castillo <cesarzc95@gmail.com>
2022-06-08 15:57:32 -0500

    cambios desde master

---
+++ testdata_backend/testdata_backend/data.py
@@ -0,0 +1,6 @@
+lista = [
+    {"name": "pc", "price": 800, "quantity": 4},
+    {"name": "laptop", "price": 1200, "quantity": 1},
+    {"name": "mouse", "price": 40, "quantity": 6},
+    {"name": "pc", "price": 600, "quantity": 1}
+]
